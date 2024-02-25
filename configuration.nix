db37fca40ebeee6360474e20140eab882d178928
Mads Mogensen <mail@madsmogensen.dk>
2024-02-25 16:47:04 +0100

    Good riddance pihole, good riddance docker
    Nobody will miss you

--- configuration.nix
+++ configuration.nix
@@ -17,7 +17,6 @@ in {
       <nixpkgs/nixos/modules/profiles/minimal.nix>
       <nixpkgs/nixos/modules/profiles/hardened.nix>
       ./minecraft.nix
-      ./pihole.nix
     ];
 
   # Use the systemd-boot EFI boot loader.
@@ -139,7 +138,7 @@ in {
     ignoreIP = [
       "127.0.0.0/8"
       "172.16.0.0/12"
-      "192.168.0.0/16"
+      "10.0.1.0/24"
     ];
     #jails = {
     #  "nginx-spam" = ''
@@ -186,22 +185,6 @@ in {
     };
   };
 
-  services.dnscrypt-proxy2 = {
-    enable = true;
-    settings = {
-      listen_addresses = [ "127.0.0.1:53" "172.17.0.1:53" ];
-      server_names = [
-        "uncensoreddns-ipv4"
-        "uncensoreddns-dk-ipv4"
-        "powerdns-doh"
-        "dns.watch"
-        "moulticast-de-ipv4"
-      ];
-      block_ipv6 = true;
-      ipv6_servers = false;
-    };
-  };
-
   security.acme.acceptTerms = true;
   security.acme.defaults.email = "mail@madsmogensen.dk";
 
@@ -238,7 +221,7 @@ in {
     virtualHosts."server-mads.lan" =  {
       # Only allow local connections to this virtual host
       extraConfig =
-          "allow 192.168.1.0/24;" +
+          "allow 10.0.1.0/24;" +
           "deny all;"
           ;
 
@@ -251,96 +234,7 @@ in {
           "proxy_pass_header Authorization;"
           ;
       };
-
-      locations."/admin" = {
-        proxyPass = "http://127.0.0.1:${toString config.services.pihole.httpPort}/admin";
-        extraConfig =
-          "proxy_set_header Host $host;" +
-          "proxy_set_header X-Script-Name /admin;" +
-          "proxy_set_header X-Real-IP $remote_addr;" +
-          "proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;" +
-          "proxy_hide_header X-Frame-Options;" +
-          "proxy_set_header X-Frame-Options \"SAMEORIGIN\";"
-          ;
-      };
     };
-  };
-
-  virtualisation.docker.autoPrune.enable = true;
-  services.pihole = {
-    enable = true;
-    version = "2023.05.2";
-    dnsPort = 5353;
-    httpPort = 9091;
-    inDns = "172.17.0.1";
-    routerDomain = "router.lan";
-    routerIp = "192.168.1.1";
-    routerCidr = "192.168.1.0/16";
-    webPassword = "toor";
-    customDnses = [
-      { ip = "192.168.1.150"; name = "server-mads.lan"; }
-    ];
-    adlists = [
-      "https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt"
-      "https://adaway.org/hosts.txt"
-      "https://v.firebog.net/hosts/AdguardDNS.txt"
-      "https://v.firebog.net/hosts/Admiral.txt"
-      "https://raw.githubusercontent.com/anudeepND/blacklist/master/adservers.txt"
-      "https://v.firebog.net/hosts/Easylist.txt"
-      "https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext"
-      "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/UncheckyAds/hosts"
-      "https://raw.githubusercontent.com/bigdargon/hostsVN/master/hosts"
-      "https://raw.githubusercontent.com/jdlingyu/ad-wars/master/hosts"
-      "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Spam/hosts"
-      "https://v.firebog.net/hosts/static/w3kbl.txt"
-      "https://raw.githubusercontent.com/matomo-org/referrer-spam-blacklist/master/spammers.txt"
-      "https://raw.githubusercontent.com/Spam404/lists/master/main-blacklist.txt"
-      "https://someonewhocares.org/hosts/zero/hosts"
-      "https://raw.githubusercontent.com/HorusTeknoloji/TR-PhishingList/master/url-lists.txt"
-      "https://v.firebog.net/hosts/Easyprivacy.txt"
-      "https://v.firebog.net/hosts/Prigent-Ads.txt"
-      "https://gitlab.com/quidsup/notrack-blocklists/raw/master/notrack-blocklist.txt"
-      "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.2o7Net/hosts"
-      "https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/spy.txt"
-      "https://hostfiles.frogeye.fr/firstparty-trackers-hosts.txt"
-      "https://hostfiles.frogeye.fr/multiparty-trackers-hosts.txt"
-      "https://raw.githubusercontent.com/Perflyst/PiHoleBlocklist/master/android-tracking.txt"
-      "https://raw.githubusercontent.com/Perflyst/PiHoleBlocklist/master/SmartTV.txt"
-      "https://raw.githubusercontent.com/Perflyst/PiHoleBlocklist/master/AmazonFireTV.txt"
-      "https://raw.githubusercontent.com/RooneyMcNibNug/pihole-stuff/master/SNAFU.txt"
-      "https://www.github.developerdan.com/hosts/lists/ads-and-tracking-extended.txt"
-      "https://raw.githubusercontent.com/DandelionSprout/adfilt/master/Alternate%20versions%20Anti-Malware%20List/AntiMalwareHosts.txt"
-      "https://osint.digitalside.it/Threat-Intel/lists/latestdomains.txt"
-      "https://s3.amazonaws.com/lists.disconnect.me/simple_malvertising.txt"
-      "https://phishing.army/download/phishing_army_blocklist_extended.txt"
-      "https://gitlab.com/quidsup/notrack-blocklists/raw/master/notrack-malware.txt"
-      "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Risk/hosts"
-      "https://urlhaus.abuse.ch/downloads/hostfile/"
-      "https://v.firebog.net/hosts/Prigent-Malware.txt"
-      "https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/gambling-porn/hosts"
-      "https://raw.githubusercontent.com/chadmayfield/my-pihole-blocklists/master/lists/pi_blocklist_porn_all.list"
-      "https://v.firebog.net/hosts/Prigent-Crypto.txt"
-      "https://zerodot1.gitlab.io/CoinBlockerLists/hosts_browser"
-    ];
-    regexes = [
-      "\\.asia$"
-      "\\.cn$"
-      "(\\.|^)huawei\\.com$"
-      "(\\.|^)open-telekom-cloud\\.com$"
-      "dbank"
-      "hicloud"
-    ];
-    blacklist = [
-      "dubaid.co.uk"
-    ];
-    whitelist = [
-      "connectivitycheck.cbg-app.huawei.com"
-      "connectivitycheck.platform.hicloud.com"
-      "fonts.gstatic.com"
-      "4chan.org"
-      "boards.4channel.org"
-      "boards.4chan.org"
-    ];
   };
 
   services.minecraft-ftb-server = {
@@ -441,8 +335,8 @@ in {
 
 
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 53 80 443 config.services.pihole.dnsPort config.services.davmail.config.davmail.imapPort config.services.davmail.config.davmail.smtpPort 25566 ];
-  networking.firewall.allowedUDPPorts = [ 53 80 443 config.services.pihole.dnsPort config.networking.wireguard.interfaces."wg0".listenPort 25566 ];
+  networking.firewall.allowedTCPPorts = [ 80 443 config.services.davmail.config.davmail.imapPort config.services.davmail.config.davmail.smtpPort 25566 ];
+  networking.firewall.allowedUDPPorts = [ 80 443 config.networking.wireguard.interfaces."wg0".listenPort 25566 ];
 
   system.autoUpgrade = {
     enable = true;
